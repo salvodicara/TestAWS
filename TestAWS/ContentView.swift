67371ae921f3c4464bb16931351b332d08f97d62
Salvatore Di Cara <salvodicara@gmail.com>
2020-07-22 15:12:18 +0100

    Initial Commit

---
+++ TestAWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestAWS
+//
+//  Created by Salvatore Di Cara on 22/07/2020.
+//  Copyright © 2020 Salvatore Di Cara. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
